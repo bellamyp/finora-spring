2b3f2c8bdd5fb3a02df548286a767f675a9f2a01
BellamyPhan <bellamyphan@icloud.com>
2025-10-26 22:01:24 -0500

    Initial commit spring backend

---
+++ src/main/java/com/bellamyphan/finora_spring/FinoraSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.finora_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinoraSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinoraSpringApplication.class, args);
+	}
+
+}
