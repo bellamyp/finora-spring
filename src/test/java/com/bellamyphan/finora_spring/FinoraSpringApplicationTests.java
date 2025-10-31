674d03055cbf7b892b03419383c151eff77f6934
BellamyPhan <bellamyphan@icloud.com>
2025-10-30 21:47:16 -0500

    Add testing configuration because of email service

--- src/test/java/com/bellamyphan/finora_spring/FinoraSpringApplicationTests.java
+++ src/test/java/com/bellamyphan/finora_spring/FinoraSpringApplicationTests.java
@@ -2,12 +2,15 @@ package com.bellamyphan.finora_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // loads application-test.properties
 class FinoraSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Real EmailService and NotificationService beans are loaded,
+        // but emails won't actually go anywhere because host=localhost
+    }
 }
