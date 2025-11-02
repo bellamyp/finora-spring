fa454d07e698f20921d9efa0900f5c7445254d88
bellamyphan <bellamyphan@icloud.com>
2025-11-02 00:06:30 -0500

    User entity refactor, update unit tests

--- src/main/java/com/bellamyphan/finora_spring/entity/User.java
+++ src/main/java/com/bellamyphan/finora_spring/entity/User.java
@@ -15,29 +15,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name")
-    private String firstName;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "username", unique = true, nullable = false)
-    @NotBlank(message = "Username is required")
-    private String username;
+    @Column(name = "email", unique = true, nullable = false)
+    @NotBlank(message = "Email is required")
+    private String email;
 
     @Column(name = "password", nullable = false)
     @NotBlank(message = "Password is required")
     private String password;
 
-    @ManyToOne(fetch = FetchType.EAGER) // usually EAGER for role
+    @ManyToOne(fetch = FetchType.EAGER) // usually EAGER for the role
     @JoinColumn(name = "role_id", nullable = false) // FK column
     private Role role;
 
     // Constructor without id
-    public User(String firstName, String lastName, String username, String password, Role role) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
+    public User(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
