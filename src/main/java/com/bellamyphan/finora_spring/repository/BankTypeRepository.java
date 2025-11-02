b7f771f62b02afc366fb5f122bb8cd0d1b7e80ab
bellamyphan <bellamyphan@icloud.com>
2025-11-02 17:52:55 -0600

    BankController: create BankTypeRepository to get bank type from DB

---
+++ src/main/java/com/bellamyphan/finora_spring/repository/BankTypeRepository.java
@@ -0,0 +1,14 @@
+package com.bellamyphan.finora_spring.repository;
+
+import com.bellamyphan.finora_spring.entity.BankType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BankTypeRepository extends JpaRepository<BankType, Long> {
+    // basic CRUD included
+
+    Optional<BankType> findByType(String type);
+}
