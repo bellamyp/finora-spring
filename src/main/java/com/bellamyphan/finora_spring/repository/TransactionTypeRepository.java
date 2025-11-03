124849a23031c30e8ebd4a812b7e51131d3e49dd
Bellamy Phan <55559722+bellamyp@users.noreply.github.com>
2025-11-02 22:28:03 -0600

    Fn 115 user be able to create a new transaction (#30)
    * Maven pom: Update testing coverage
    
    * TransactionController: write POST call to create a new transaction

---
+++ src/main/java/com/bellamyphan/finora_spring/repository/TransactionTypeRepository.java
@@ -0,0 +1,14 @@
+package com.bellamyphan.finora_spring.repository;
+
+import com.bellamyphan.finora_spring.entity.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TransactionTypeRepository extends JpaRepository<TransactionType, Long> {
+    // JpaRepository provides basic CRUD operations
+
+    Optional<TransactionType> findByType(String type); // lookup by type string
+}
