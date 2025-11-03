1c3c664c6bf0b4ac1b3ac92dc183ea4ca8d72ab4
bellamyphan <bellamyphan@icloud.com>
2025-11-03 17:13:56 -0600

    Update Bank entity, enum, repository, controller to use UUID

--- src/main/java/com/bellamyphan/finora_spring/repository/BankRepository.java
+++ src/main/java/com/bellamyphan/finora_spring/repository/BankRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface BankRepository extends JpaRepository<Bank, Long> {
+public interface BankRepository extends JpaRepository<Bank, UUID> {
     // basic CRUD included
 
     List<Bank> findByUser_Email(String email);
