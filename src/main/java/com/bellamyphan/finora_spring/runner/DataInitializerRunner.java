b98b048bff35b6855c86144d94a3eae11e8ad61c
BellamyPhan <bellamyphan@icloud.com>
2025-11-26 02:43:26 -0600

    DataInitializerRunner > can seed transactionTypes

--- src/main/java/com/bellamyphan/finora_spring/runner/DataInitializerRunner.java
+++ src/main/java/com/bellamyphan/finora_spring/runner/DataInitializerRunner.java
@@ -1,8 +1,11 @@
 package com.bellamyphan.finora_spring.runner;
 
 import com.bellamyphan.finora_spring.constant.RoleEnum;
+import com.bellamyphan.finora_spring.constant.TransactionTypeEnum;
 import com.bellamyphan.finora_spring.entity.Role;
+import com.bellamyphan.finora_spring.entity.TransactionType;
 import com.bellamyphan.finora_spring.service.RoleService;
+import com.bellamyphan.finora_spring.service.TransactionTypeService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,22 +18,36 @@ public class DataInitializerRunner implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(DataInitializerRunner.class);
     private final RoleService roleService;
+    private final TransactionTypeService transactionTypeService;
 
     @Override
     public void run(String... args) {
         initRoles();
+        initTransactionTypes();
     }
 
     private void initRoles() {
         for (RoleEnum roleEnum : RoleEnum.values()) {
-            String roleName = roleEnum.name(); // get enum name as string
             if (!roleService.existsByName(roleEnum)) {
                 Role role = new Role();
                 role.setName(roleEnum);
                 roleService.save(role);
-                logger.info("✅ Role created: {}", roleName);
+                logger.info("✅ Role created: {}", roleEnum.name());
             } else {
-                logger.info("ℹ️ Role already exists: {}", roleName);
+                logger.info("ℹ️ Role already exists: {}", roleEnum.name());
+            }
+        }
+    }
+
+    private void initTransactionTypes() {
+        for (TransactionTypeEnum typeEnum : TransactionTypeEnum.values()) {
+            if (!transactionTypeService.existsByName(typeEnum)) {
+                TransactionType type  = new TransactionType();
+                type.setType(typeEnum);
+                transactionTypeService.save(type);
+                logger.info("✅ Transaction type created: {}", typeEnum.name());
+            } else {
+                logger.info("ℹ️ Transaction type already exists: {}", typeEnum.name());
             }
         }
     }
