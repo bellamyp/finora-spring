6ab038d4b674591c60635c3f46d078e4bcb1095f
Bellamy Phan <55559722+bellamyp@users.noreply.github.com>
2025-11-29 21:15:08 -0600

    Fn 333 list banks together by group (#54)
    * BankController can now return GroupId with the bank details
    
    * BankController > Return a list of bank will be sort by BankGroup.name
    
    * Update unit tests for BankController and BankService

--- src/main/java/com/bellamyphan/finora_spring/dto/BankDto.java
+++ src/main/java/com/bellamyphan/finora_spring/dto/BankDto.java
@@ -1,30 +1,20 @@
 package com.bellamyphan.finora_spring.dto;
 
 import com.bellamyphan.finora_spring.constant.BankTypeEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BankDto {
     private String id;
+    private String groupId;
     private String name;
-    private BankTypeEnum type; // now returns enum
+    private BankTypeEnum type;
     private String email;
     private BigDecimal balance;
-
-    public BankDto(String id, String name, BankTypeEnum type, String email) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.email = email;
-    }
-
-    public BankDto(String id, String name, BankTypeEnum type, String email, BigDecimal balance) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.email = email;
-        this.balance = balance;
-    }
 }
