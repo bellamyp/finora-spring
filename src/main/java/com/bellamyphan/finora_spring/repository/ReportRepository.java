687e88e9d0e7635710721a50b18f4f8409c5961a
BellamyPhan <bellamyphan@icloud.com>
2026-01-04 11:57:48 -0600

    ReportRepository fix small annotations

--- src/main/java/com/bellamyphan/finora_spring/repository/ReportRepository.java
+++ src/main/java/com/bellamyphan/finora_spring/repository/ReportRepository.java
@@ -1,25 +1,26 @@
 package com.bellamyphan.finora_spring.repository;
 
 import com.bellamyphan.finora_spring.entity.Report;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, String> {
-    // Find latest report for a user
-    Optional<Report> findTopByUserIdOrderByMonthDesc(String userId);
 
-    Optional<Report> findById(@NotNull String id);
+    // Find latest report for a user
+    Optional<Report> findTopByUserIdOrderByMonthDesc(@NonNull String userId);
+
+    Optional<Report> findById(@NonNull String id);
 
     // All reports for a user, sorted by month descending
-    List<Report> findAllByUserIdOrderByMonthDesc(String userId);
+    List<Report> findAllByUserIdOrderByMonthDesc(@NonNull String userId);
 
     // Check if user has at least 1 pending report
-    boolean existsByUserIdAndIsPostedFalse(String userId);
+    boolean existsByUserIdAndIsPostedFalse(@NonNull String userId);
 
     // --------------------------
     // Live calculation of type balances
@@ -31,5 +32,5 @@ public interface ReportRepository extends JpaRepository<Report, String> {
         WHERE tg.report.id = :reportId
         GROUP BY t.type.id
     """)
-    List<ReportTypeAggregate> calculateLiveTypeBalances(@Param("reportId") String reportId);
+    List<ReportTypeAggregate> calculateLiveTypeBalances(@Param("reportId") @NonNull String reportId);
 }
